0e2173e715322c6d9e8fb210021e4afadad69295
Jayden Vause <jaydenvausemailbox@gmail.com>
2024-06-09 17:02:00 +1000

    fixed invoicing page

--- resources/views/invoices.blade.php
+++ resources/views/invoices.blade.php
@@ -48,18 +48,19 @@
 
 <x-layout>
 
-
+<!--
 <div class="cont">
     <h1>Search Invoice</h1>
     <form class="form" action="/invoices" method="POST">
-        @csrf <!-- Add CSRF token for form submission -->
+        @csrf
         <label for="invoice_id">Invoice ID</label>
         <input type="text" name="invoice_id" placeholder="e.g A2031293232" />
         <label for="security_key">Security Key</label>
         <input type="password" name="security_key" placeholder="********" />
-        <button type="submit">Search</button> <!-- Specify type="submit" for the button -->
+        <button type="submit">Search</button> 
     </form>
 </div>
+-->
 
 <div class="cont">
     <h1>Admin Panel</h1>
